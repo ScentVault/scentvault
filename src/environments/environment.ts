d9b320fbc6f7d181dc4f5e44b4425b7060fd1c1f
Patricio Hevia <patricio.hevia@ug.uchile.cl>
2024-07-29 19:50:23 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyA5uxJW4ZZFQeHnx0xFkfMMhqudO77ac2s",
+        authDomain: "scent-vault.firebaseapp.com",
+        projectId: "scent-vault",
+        storageBucket: "scent-vault.appspot.com",
+        messagingSenderId: "76073742559",
+        appId: "1:76073742559:web:1b0f9576a3868ed810f96a",
+        measurementId: "G-44RRQZZNVW"
+    }
+};
